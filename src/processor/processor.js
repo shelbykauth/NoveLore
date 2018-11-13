8bba51acfe56e636490a5b71a0ca8e5de5bb1dbe
Shelby Kauth <shelbykauth@gmail.com>
2018-11-12 20:16:05 -0500

    Cleanup Unused Functions

--- src/processor/processor.js
+++ src/processor/processor.js
@@ -4,42 +4,6 @@ const shortid = require('shortid');
 
 const inputFiles = require("./takeInputs.js");
 
-function processRawInput() {
-    var InputDir = path.join(dataRoot, "/input/");
-
-    return new Promise(function(resolve, reject) {
-        fs.readdir(InputDir, (err, files) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(files);
-            }
-        })
-    }).then((files) => {
-        var proms = [];
-        for (var n in files) {
-            var path = files[n];
-            if (path.match(/\.zip$/)) {
-                proms.push(new Promise(function() {
-                    console.log(path);
-                }));
-            }
-        }
-        return new PromiseAll([]);
-    });
-}
-
-function processFile(relPath) {
-    return new Promise(function(resolve, reject) {
-        var fullPath = path.join(dataRoot, "/input/", relPath);
-
-    });
-}
-
-function processNextFile() {
-
-}
-
 module.exports = {
     inputFiles: inputFiles.takeInputs,
     unzip: inputFiles.unzip,
