c3bb806ffb796ff1f55bfea57fbccf26c0d2eb5f
ShelbyKauth <shelbykauth@gmail.com>
2019-11-14 19:42:04 -0500

    Prevent eslint spam in `npm run serve:client`

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "lint-on-save": false
   },
   parserOptions: {
     parser: "babel-eslint"
